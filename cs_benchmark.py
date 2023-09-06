853b4a7aab7b729b31abe0e56959809c37263d61
Marvin <marvnsch@campus.uni-paderborn.de>
2023-09-06 18:44:24 +0200

    add benchmark adjustments

--- cs_benchmark.py
+++ cs_benchmark.py
@@ -3,12 +3,16 @@
 import socket
 import time
 import sys
-import random
+import os
 
 PORT = 5000
 SIZE = 20 # Anzahl an Knoten
 RECEIVER = 2 # ID of the node, that should receive the message
 SENDER = 15 # ID of the node, that sends the message
+
+# netsimlan starten
+os.popen("netsimlan project -a 5000 -i 2000 -v off -g off -l run.log")
+time.sleep(5)
 
 def raw_api_query(reader, writer, command):
     """
@@ -47,39 +51,49 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
 
         # Register the sender to all nodes
         for i in range (1, SIZE + 1):
-            if (i != SENDER):
-                raw_api_query(r, w, "call " + str(i) + " setSender " + str(SENDER))
+            raw_api_query(r, w, "call " + str(i) + " setSender " + str(SENDER))
 
-        time.sleep(2)
+        time.sleep(10)
 
         benchmark_end = False
         print("### START BENCHMARK ###")
         startTime = time.time()
         
         # Generate messages
-        for i in range (0):
+        for i in range (5):
             for i in range(1, SIZE + 1):
                 if (i != SENDER):
                     raw_api_query(r, w, "call " + str(i) + " insert 42")
 
         raw_api_query(r, w, "call " + str(SENDER) + " insertQueue 42")
 
-        for i in range(0):
+        for i in range(5):
             for i in reversed(range(SIZE + 1, 1)):
                 if (i != SENDER):
                     raw_api_query(r, w, "call " + str(i) + " insert 42")
 
+        # get log file path
+        absolute_path = os.path.dirname(__file__)
+        relative_path = "run.log"
+        full_path = os.path.join(absolute_path, relative_path)
 
-        while (time.time() - startTime < 10):
-            line = r.readline().strip('\n')
-            if (line == "BENCHMARK STOP"):
-                endTime = time.time()
-                benchmark_end = True
-                break
+        # setup regex
+        import re
+        regex = re.compile('.*BENCHMARK STOP.*')
+
+        while (time.time() - startTime < 15):
+            with open(full_path) as f:
+                log_text = f.read()
+                if (regex.search(log_text)):
+                    endTime = time.time()
+                    benchmark_end = True
+                    break
             time.sleep(0.01)
-    # close connection
-    raw_api_query(r, w, "exit")
+        # close connection
+        raw_api_query(r, w, "exit")
+        os.popen("kill $(ps -e | grep netsimlan | awk '{print $1}')")
+        os.popen("kill $(ps -e | grep java | awk '{print $1}')")
 if (benchmark_end == True):
-    print("TIME: " + str((endTime - startTime) / 1000))
+    print("TIME: " + str((endTime - startTime)))
 else:
     print("Error: TIMEOUT")
